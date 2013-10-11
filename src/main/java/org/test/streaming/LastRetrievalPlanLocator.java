555cde6ee7a3fca02f98ecef27df684d1073bbbe
Matias Eidis <matiaseidis@gmail.com>
2013-10-11 18:15:41 -0300

    bug fix en key de progress

--- src/main/java/org/test/streaming/LastRetrievalPlanLocator.java
+++ src/main/java/org/test/streaming/LastRetrievalPlanLocator.java
@@ -11,19 +11,20 @@ import org.apache.commons.logging.LogFactory;
 import org.test.streaming.status.CachoProgress;
 
 public class LastRetrievalPlanLocator {
-	
+
 	protected static final Log log = LogFactory.getLog(LastRetrievalPlanLocator.class);
-	
+
 	private static final LastRetrievalPlanLocator instance = new LastRetrievalPlanLocator();
 	private final List<CachoStreamer> streamers = new ArrayList<CachoStreamer>();
-	
-//	private final Map<CachoRequest, ProgressReport> progress = new HashMap<CachoRequest, ProgressReport>();
+
+	// private final Map<CachoRequest, ProgressReport> progress = new
+	// HashMap<CachoRequest, ProgressReport>();
 	private final Map<String, CachoProgress> progress = new HashMap<String, CachoProgress>();
-	
-	private LastRetrievalPlanLocator(){
+
+	private LastRetrievalPlanLocator() {
 		log.debug("LastRetrievalPlanLocator created");
 	}
-	
+
 	public static LastRetrievalPlanLocator getInstance() {
 		return instance;
 	}
@@ -35,8 +36,8 @@ public class LastRetrievalPlanLocator {
 	/**
 	 * Cierro los streams del plan anterior
 	 */
-	public void clean(){
-		for(CachoStreamer sc : this.getStreamers()) {
+	public void clean() {
+		for (CachoStreamer sc : this.getStreamers()) {
 			try {
 				log.debug("About to close stream...");
 				sc.close();
@@ -59,48 +60,48 @@ public class LastRetrievalPlanLocator {
 		return progress;
 	}
 
-//	public void registerCacho(CachoRequest request) {
-//		log.debug("register cacho http");
-//		CachoProgress cp = new CachoProgress(request, 
-//				new ProgressReport(request.getCacho()));
-//		this.getProgress().put(progressKey(cp), cp);
-//		
-//	}
+	// public void registerCacho(CachoRequest request) {
+	// log.debug("register cacho http");
+	// CachoProgress cp = new CachoProgress(request,
+	// new ProgressReport(request.getCacho()));
+	// this.getProgress().put(progressKey(cp), cp);
+	//
+	// }
 
 	public static String progressKey(CachoProgress cp) {
-		return progressKey(cp.getFirstByteIndex(), cp.getFirstByteIndex()+cp.getLength());
+		return progressKey(cp.getFirstByteIndex(), cp.getLength());
 	}
 
-//	public void updateCachoProgress(CachoRequest key, ProgressReport value) {
-//
-//		CachoProgress target = null;
-//		for(CachoProgress cp : this.getProgress().values()) {
-//			if(cp.getCachoRequest().getFirstByteIndex() == key.getFirstByteIndex()
-//					&& cp.getCachoRequest().getLength() == key.getLength()) {
-//				target = cp;
-//				break;
-//			}
-//		}
-//		if(target == null) {
-//			log.error("target null. this should not happen");
-//			return;
-//		}
-//		target.setProgressReport(value);
-//	}
+	// public void updateCachoProgress(CachoRequest key, ProgressReport value) {
+	//
+	// CachoProgress target = null;
+	// for(CachoProgress cp : this.getProgress().values()) {
+	// if(cp.getCachoRequest().getFirstByteIndex() == key.getFirstByteIndex()
+	// && cp.getCachoRequest().getLength() == key.getLength()) {
+	// target = cp;
+	// break;
+	// }
+	// }
+	// if(target == null) {
+	// log.error("target null. this should not happen");
+	// return;
+	// }
+	// target.setProgressReport(value);
+	// }
 
 	public CachoProgress getProgressFor(int firstByteIndex, int length) {
-		
+
 		CachoProgress p = this.getProgress().get(this.progressKey(firstByteIndex, length));
-		
-		if(p == null) {
+
+		if (p == null) {
 			p = new CachoProgress(firstByteIndex, length);
 			this.getProgress().put(progressKey(p), p);
 		}
-		
+
 		return p;
 	}
 
 	private static String progressKey(int firstByteIndex, int length) {
-		return firstByteIndex +"-"+length;
+		return firstByteIndex + "-" + length;
 	}
 }
